a635e674d558c605f1dbd8b9a019b6d5be5158a4
blackb1rd <blackb1rd.mov@gmail.com>
2015-01-05 23:50:43 +0700

    Update script for installing dot files

--- setup.sh
+++ setup.sh
@@ -1,11 +1,85 @@
-use strict;
-use warnings;
+#!/bin/bash
 
-if (`tput colors` != 256) {
-    system('apt-get install ncurses-term');
-    open  FILE ,">>~/.bashrc";
-    print FILE "\nexport TERM=xterm-256color";
-    close FILE;
-    system('source ~/.bashrc');
-}
-print "check 256 color mode completed\n";
+# Get the current directory
+current_dir="$( cd "$( dirname "$0" )" && pwd )"
+
+ncolors=$(tput colors)
+if [[ $ncolors != 256 ]]
+then
+  apt-get install ncurses-term
+fi
+
+if [[ ! -a ~/.zshrc ]]
+then
+  ln -s $current_dir/shell/zshrc $HOME/.zshrc
+fi
+
+if [[ ! -a ~/.bashrc ]]
+then
+  ln -s $current_dir/shell/bashrc $HOME/.bashrc
+fi
+
+if [[ ! -a ~/.gdbrc ]]
+then
+  ln -s $script_dir/debugger/gdbrc $HOME/.gdbrc
+fi
+
+if [[ ! -a ~/.cgdb/cgdbrc ]]
+then
+mkdir $HOME/.cgdb
+  ln -s $script_dir/debugger/cgdbrc $HOME/.cgdb/cgdbrc
+fi
+
+###############################################################################
+#                                   _                                         #
+#                            __   _(_)_ __ ___                                #
+#                            \ \ / / | '_ ` _ \                               #
+#                             \ V /| | | | | | |                              #
+#                              \_/ |_|_| |_| |_|                              #
+#                                                                             #
+###############################################################################
+
+if [[ ! -a ~/.vimrc ]]
+then
+  ln -s $script_dir/vim/vimrc $HOME/.vimrc
+fi
+
+if [[ ! -d "$HOME/.vim" ]]
+then
+  mkdir "$HOME/.vim"
+fi
+
+if [[ ! -d "$HOME/.vim/tmp" ]]
+then
+  mkdir "$HOME/.vim/tmp"
+fi
+
+if [[ ! -d "$HOME/.vim/backups" ]]
+then
+  mkdir "$HOME/.vim/backups"
+fi
+
+if [[ ! -d "$HOME/.vim/undo" ]]
+then
+  mkdir "$HOME/.vim/undo"
+fi
+
+# Install bundle
+cp -r $script_dir/vim/bundle "$HOME/.vim/"
+
+# Install YouCompleteMe
+cd "$HOME/.vim/bundle"
+cd YouCompleteMea && ./install.sh
+
+# Install Taglist
+cd ..
+wget http://www.vim.org/scripts/download_script.php?src_id=19574  -O taglist_46.zip
+unzip -o taglist_46.zip -d "$HOME/.vim/bundle/taglist/"
+rm taglist_46.zip
+
+# Install fonts power line
+git clone https://github.com/powerline/fonts.git
+cd fonts && ./install.sh
+cd .. && rm -r fonts
+
+#TODO create symbolic link for folder
