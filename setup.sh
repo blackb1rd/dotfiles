aafaae4c1fbf218e50137d8baac362eb5d087e9b
blackb1rd <blackb1rd.mov@gmail.com>
2018-03-18 03:11:07 +0700

    change to use pyenv as defalut

--- setup.sh
+++ setup.sh
@@ -2,21 +2,153 @@
 
 GITHUB_RAW_URL='https://raw.githubusercontent.com'
 GITHUB_URL='https://github.com'
-PACKAGE="cmake
-         figlet
-         git
-         htop
-         irssi
-         lynx
-         nodejs
-         tmux
-	 xclip
-         zsh"
-
-        #lua
-        #gocode
-        #lua-dev
 TEMP="/tmp"
+
+os_release_id="$(grep -E '^ID=([a-zA-Z]*)' /etc/os-release | cut -d '=' -f 2)"
+case $(uname) in
+  Darwin)
+    ;;
+  CYGWIN_NT-*)
+    ;;
+  MSYS_NT-*)
+    PKG_CMD_UPDATE="pacman -Sy"
+    PKG_CMD_INSTALL="pacman -Suu"
+    PKG_CMD_REMOVE="pacman -R"
+    ;;
+  FreeBSD)
+    ;;
+  OpenBSD)
+    ;;
+  DragonFly)
+    ;;
+  Linux)
+    case "$os_release_id" in
+      "arch")
+        ;;
+      "debian")
+        PKG_CMD_UPDATE="sudo apt-get update"
+        PKG_CMD_INSTALL="sudo apt-get install -y"
+        PKG_CMD_REMOVE="sudo apt-get remove -y"
+        PACKAGE="autoconf
+                 automake
+                 build-essential
+                 cmake
+                 curl
+                 figlet
+                 g++
+                 git
+                 htop
+                 irssi
+                 libbz2-dev
+                 libncurses5-dev
+                 libreadline-dev
+                 libsqlite3-dev
+                 libssl1.0-dev
+                 libtool
+                 llvm
+                 lynx
+                 make
+                 ninja-build
+                 nodejs
+                 pkg-config
+                 python-dev
+                 python3-dev
+                 ruby-dev
+                 tk-dev
+                 tmux
+                 unzip
+                 wget
+                 xclip
+                 xz-utils
+                 zlib1g-dev
+                 zsh"
+        ;;
+      "ubuntu")
+        PKG_CMD_UPDATE="sudo apt-get update"
+        PKG_CMD_INSTALL="sudo apt-get install -y"
+        PKG_CMD_REMOVE="sudo apt-get remove -y"
+        PACKAGE="autoconf
+                 automake
+                 build-essential
+                 cmake
+                 curl
+                 figlet
+                 g++
+                 git
+                 htop
+                 irssi
+                 libbz2-dev
+                 libncurses5-dev
+                 libreadline-dev
+                 libsqlite3-dev
+                 libssl1.0-dev
+                 libtool
+                 llvm
+                 lynx
+                 make
+                 ninja-build
+                 nodejs
+                 pkg-config
+                 python-dev
+                 python3-dev
+                 ruby-dev
+                 tk-dev
+                 tmux
+                 unzip
+                 wget
+                 xclip
+                 xz-utils
+                 zlib1g-dev
+                 zsh"
+        ;;
+      "elementary")
+        ;;
+      "fedora")
+        PKG_CMD_UPDATE="sudo yum update"
+        PKG_CMD_INSTALL="sudo yum install -y"
+        PKG_CMD_REMOVE="sudo yum remove -y"
+        ;;
+      "coreos")
+        ;;
+      "gentoo")
+        ;;
+      "mageia")
+        ;;
+      "centos")
+        PKG_CMD_UPDATE="sudo yum update"
+        PKG_CMD_INSTALL="sudo yum install -y"
+        PKG_CMD_REMOVE="sudo yum remove -y"
+        ;;
+      "opensuse"|"tumbleweed")
+        ;;
+      "sabayon")
+        ;;
+      "slackware")
+        ;;
+      "linuxmint")
+        PKG_CMD_UPDATE="sudo apt-get update"
+        PKG_CMD_INSTALL="sudo apt-get install -y"
+        PKG_CMD_UPDATE="sudo apt-get remove -y"
+        ;;
+      *)
+        ;;
+    esac
+
+    # Check if we're running on Android
+    case $(uname -o 2>/dev/null) in
+      Android)
+        PKG_CMD_UPDATE="pkg update"
+        PKG_CMD_INSTALL="pkg install"
+        PKG_CMD_INSTALL="pkg uninstall"
+        ;;
+    esac
+    ;;
+  SunOS)
+    ;;
+  *)
+    ;;
+esac
+
 usage() {
   echo "Usage: $0 [options]"
   echo ""
@@ -121,30 +253,12 @@ fi
 if [ -n "${all}" ] || [ -n "${basictool}" ] ; then
   echo "${txtbld}$(tput setaf 1)[-] Install the basic tool$(tput sgr0)"
   if [ $OStype = "linux" ] ; then
-    # Find the DISTRIB
-    DISTRIB=$(lsb_release -si | awk '{print tolower($0)}')
-
-    if [ $DISTRIB = "ubuntu" ] || [ $DISTRIB = "debian" ] ; then
-      PACKAGE="$PACKAGE
-               autoconf
-               automake
-               build-essential
-               g++
-               libtool
-               ninja-build
-               pkg-config
-               python-dev
-               python3-dev
-               ruby-dev
-               unzip"
-      sudo apt-get update
-      sudo apt-get install -y $PACKAGE \
-                              || { echo 'Failed to install program' ; exit 1; }
-      # if did not want to install latest version
-      if [ ! "${latest}" ] && [ ! "${all}" ] ; then
-        sudo apt-get install -y vim ctags
-      fi
-
+    $PKG_CMD_UPDATE
+    $PKG_CMD_INSTALL $PACKAGE \
+                            || { echo 'Failed to install program' ; exit 1; }
+    # if did not want to install latest version
+    if [ ! "${latest}" ] && [ ! "${all}" ] ; then
+      $PKG_CMD_INSTALL vim ctags
     fi
   elif [ $OStype = "termux" ] ; then
       pkg install -y $PACKAGE || { echo 'Failed to install program' ; exit 1; }
@@ -167,7 +281,7 @@ current_dir="$( cd "$( dirname "$0" )" && pwd )"
 if [ -n "${all}" ] || [ -n "${latest}" ] ; then
   if [ $OStype = "linux" ] \
      || [ $OStype = "termux" ] ; then
-    sudo apt-get remove -y ctags
+    $PKG_CMD_REMOVE ctags
 
     # clone ctags
     git clone --depth 1 $GITHUB_URL/universal-ctags/ctags $TEMP/ctags
@@ -315,25 +429,26 @@ if [ -n "${all}" ] || [ -n "${dot}" ] || [ -n "${python}" ] ; then
                pandas
                tensorflow"
 
-    # try to install pip module from python3 only
-    PythonVer=$(command -v pip > /dev/null 2>&1 && pip -V | awk -F '[()]' '{ print $2 }' | awk '{print $2}' | cut -d '.' -f 1)
-    PythonVer3=$(command -v pip3 > /dev/null 2>&1 && pip3 -V | awk -F '[()]' '{ print $2 }' | awk '{print $2}' | cut -d '.' -f 1)
-
-    if [ $(command -v pip > /dev/null 2>&1) ] && [ $PythonVer = "3" ] ; then
-      pip $PIPoption $PIPmodule
-    elif [ $(command -v pip3 > /dev/null 2>&1) ] && [ $PythonVer3 = "3" ] ; then
-      pip3 $PIPoption $PIPmodule
-    else
-      wget https://bootstrap.pypa.io/get-pip.py -O $TEMP/get-pip.py
-      sudo python3 $TEMP/get-pip.py
-      pip $PIPoption $PIPmodule
-    fi
-
     # install pyenv
     curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
+    pyenv install -s 2.7.11
+    pyenv install -s 3.6.4
+
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+    pyenv virtualenv 2.7.11 neovim2
+    pyenv virtualenv 3.6.4 neovim3
+
+    pyenv activate neovim2
+    pip install $PIPmodule
+
+    pyenv activate neovim3
+    pip install $PIPmodule
+
     # set pyenv to system
-    pyenv shell system
+    pyenv shell 3.6.4
+    pyenv global 3.6.4
   fi
 fi
 
@@ -427,7 +542,7 @@ if [ -n "${all}" ] \
      || [ $OStype = "termux" ] ; then
     if [ -n "${all}" ] || [ -n "${latest}" ] ; then
       # Install latest vim version
-      sudo apt-get remove -y vim
+      $PKG_CMD_REMOVE vim
 
       echo "${txtbld}$(tput setaf 1)[-] Install the latest VIM$(tput sgr0)"
 
@@ -445,10 +560,11 @@ if [ -n "${all}" ] \
     fi
     if [ -n "${all}" ] || [ -n "${dot}" ] ; then
       mkdirfolder .vim
-      mkdirfolder .vim/tmp
-      mkdirfolder .vim/backups
-      mkdirfolder .vim/undo
       mkdirfolder .vim/
+      mkdirfolder .vim/backups
+      mkdirfolder .vim/tmp
+      mkdirfolder .vim/undo
+
       mkdirfolder .config/nvim
 
       if [ ! -d "$HOME/github/dotfiles/vim/bundle/Vundle.vim" ] ; then
@@ -463,12 +579,21 @@ if [ -n "${all}" ] \
       # download all plugin
       nvim +PluginInstall +qall
 
+      # vim
+      # keep these confiure if use original vim
       installfile .vim/dict.add vim/dict.add
       installfile .vim/filetype.vim vim/filetype.vim
-      installfolder vim/spell
       installfile .vimrc vim/vimrc
-      installfile .config/nvim/init.vim vim/vimrc
+      installfolder vim/spell
       installfolder vim/ycm
+
+      # neovim
+      installfile .config/nvim/dict.add vim/dict.add
+      installfile .config/nvim/filetype.vim vim/filetype.vim
+      installfile .config/nvim/init.vim vim/vimrc
+      installfolder config/nvim/spell
+      installfolder config/nvim/ycm
+
     fi
     if [ -n "${all}" ] || [ -n "${ycmd}" ] ; then
       # Install YouCompleteMe
