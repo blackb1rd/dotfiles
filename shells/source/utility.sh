81d20ef0281a5e310a59f95442266fb717319d57
blackb1rd <blackb1rd.mov@gmail.com>
2019-05-21 12:58:19 +0700

    Update : myupdate

--- shells/source/utility.sh
+++ shells/source/utility.sh
@@ -9,8 +9,40 @@ pathadd() {
 
 myupdate()
 {
-  sudo apt-get update && sudo apt-get -y upgrade
-  nvim +PlugInstall +qall
-  nvim +PlugUpdate +qall
-  antigen update
+  # Check argument
+  while [ $# != 0 ]
+  do
+    case $1 in
+      -d    | --dev )          development=true;;
+      -p    | --pro )          production=true;;
+      * )                      myupdateusage;exit 1
+    esac
+    shift
+  done
+
+  if [ -z "${development}" ] \
+     && [ -z "${production}" ] ; then
+
+    echo "Need more option(development or production) to be set"
+    echo ""
+    myupdateusage
+  else
+    sudo apt-get update && sudo apt-get -y upgrade
+    nvim +PlugInstall +qall
+    nvim +PlugUpdate +qall
+    antigen update
+
+    if [ -n "${development}" ] ; then
+      flutter upgrade
+      flutter update-packages
+    fi
+  fi
+}
+
+myupdateusage() {
+  echo "Usage: myupdate [options]"
+  echo ""
+  echo "Options:"
+  echo "  -d,    --dev  Development update"
+  echo "  -p,    --pro  Production update"
 }
