0cb8e993d92cd29f65e740713382c04a5359f612
blackb1rd <blackb1rd.mov@gmail.com>
2020-04-11 22:48:21 +0700

    shell : fix posix issue

--- shells/source/transmission.sh
+++ shells/source/transmission.sh
@@ -1,13 +1,13 @@
 TR_OPTS="-n transmission:transmission"
 
-tsm-daemon-start()  { sudo service transmission-daemon start;}
-tsm-daemon-stop()   { sudo service transmission-daemon stop;}
-tsm-daemon-reload() { sudo service transmission-daemon reload;}
-tsm-start()         { transmission-remote ${TR_OPTS} -t "$1" -s;}
-tsm-stop()          { transmission-remote ${TR_OPTS} -t "$1" -s;}
-tsm-add()           { transmission-remote ${TR_OPTS} -a "$1";}
-tsm-remove()        { transmission-remote ${TR_OPTS} -t "$1" -r;}
-tsm-list()          { transmission-remote ${TR_OPTS} -l;}
-tsm-info()          { transmission-remote ${TR_OPTS} -t "$1" -i;}
-tsm-basicstats()    { transmission-remote ${TR_OPTS} -st;}
-tsm-fullstats()     { transmission-remote ${TR_OPTS} -si;}
+TsmDaemonStart()  { sudo service transmission-daemon start;}
+TsmDaemonStop()   { sudo service transmission-daemon stop;}
+TsmDaemonReload() { sudo service transmission-daemon reload;}
+TsmStart()         { transmission-remote "${TR_OPTS}" -t "$1" -s;}
+TsmStop()          { transmission-remote "${TR_OPTS}" -t "$1" -s;}
+TsmAdd()           { transmission-remote "${TR_OPTS}" -a "$1";}
+TsmRemove()        { transmission-remote "${TR_OPTS}" -t "$1" -r;}
+TsmList()          { transmission-remote "${TR_OPTS}" -l;}
+TsmInfo()          { transmission-remote "${TR_OPTS}" -t "$1" -i;}
+TsmBasicstats()    { transmission-remote "${TR_OPTS}" -st;}
+TsmFullstats()     { transmission-remote "${TR_OPTS}" -si;}
