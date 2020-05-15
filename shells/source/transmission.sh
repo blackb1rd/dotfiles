16078d1dd942c3b2b6e293e0ba81207b4af99781
blackb1rd <blackb1rd.mov@gmail.com>
2020-05-16 01:29:59 +0700

    Transmission script lowercase only

--- shells/source/transmission.sh
+++ shells/source/transmission.sh
@@ -2,14 +2,14 @@
 
 TR_OPTS="-n transmission:transmission"
 
-TsmDaemonStart()  { sudo service transmission-daemon start;}
-TsmDaemonStop()   { sudo service transmission-daemon stop;}
-TsmDaemonReload() { sudo service transmission-daemon reload;}
-TsmStart()         { transmission-remote "${TR_OPTS}" -t "$1" -s;}
-TsmStop()          { transmission-remote "${TR_OPTS}" -t "$1" -s;}
-TsmAdd()           { transmission-remote "${TR_OPTS}" -a "$1";}
-TsmRemove()        { transmission-remote "${TR_OPTS}" -t "$1" -r;}
-TsmList()          { transmission-remote "${TR_OPTS}" -l;}
-TsmInfo()          { transmission-remote "${TR_OPTS}" -t "$1" -i;}
-TsmBasicstats()    { transmission-remote "${TR_OPTS}" -st;}
-TsmFullstats()     { transmission-remote "${TR_OPTS}" -si;}
+tsmdaemonstart()  { sudo service transmission-daemon start;}
+tsmdaemonstop()   { sudo service transmission-daemon stop;}
+tsmdaemonreload() { sudo service transmission-daemon reload;}
+tsmstart()         { transmission-remote "${TR_OPTS}" -t "$1" -s;}
+tsmstop()          { transmission-remote "${TR_OPTS}" -t "$1" -s;}
+tsmadd()           { transmission-remote "${TR_OPTS}" -a "$1";}
+tsmremove()        { transmission-remote "${TR_OPTS}" -t "$1" -r;}
+tsmlist()          { transmission-remote "${TR_OPTS}" -l;}
+tsminfo()          { transmission-remote "${TR_OPTS}" -t "$1" -i;}
+tsmbasicstats()    { transmission-remote "${TR_OPTS}" -st;}
+tsmfullstats()     { transmission-remote "${TR_OPTS}" -si;}
